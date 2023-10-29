4740a818d541575445e6743790d9e89b09b828e3
Shubhi Chourey <shubhi010503@gmail.com>
2023-10-29 22:49:09 +0530

    using html css js

---
+++ shoppinglist/script.js
@@ -0,0 +1,37 @@
+const inputBox = document.getElementById("box");
+const listContainer=document.getElementById("list");
+
+
+function addTask(){
+    if(inputBox.value=== ''){
+        alert("Enter atleast one item!");
+    }
+    else{
+        let li= document.createElement("li");
+        li.innerHTML  = inputBox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement('span');
+        span.innerHTML = "\u00d7";//code for cross icon
+        li.appendChild(span)
+    } 
+    inputBox.value="";
+    saveData();
+}
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==='SPAN'){
+        e.target.parentElement.remove();
+        saveData(); 
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showItems(){
+    listContainer.innerHTML =localStorage.getItem("data");
+}
+showItems();
